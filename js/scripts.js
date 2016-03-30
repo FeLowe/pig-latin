26bdc7ea1f14884595da0fdce7cc0c3e6c3d1883
Fernanda Lowe and Jeffrey Hackford <jeffrey@>
2016-03-30 09:13:24 -0700

    add a refactor to our code, making it into one line as return is both false/true

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,21 @@
 $(document).ready(function (){
   $("form#leap-year").submit(function(event){
-      event.preventDefault();
+    event.preventDefault();
+
+    var year = parseInt($("input#year").val());
+    var result = leapYear(year);
+    $("#result").text(result);
 
   });
+    var leapYear = function(year){
+      if ((year % 4 === 0) && (year % 100 !== 0)){
+        return true;
+      } else {
+        return false;
+      }
+
+
+
+    };
+
 });
